c10fabd2edcbe6d9aa66e0aa51695c2d7ecb7780
Tribeam <57662310+Tribeam@users.noreply.github.com>
2020-09-20 21:30:14 -0500

    Communication with lexicon is go.

--- pk3/ACS/main.c
+++ pk3/ACS/main.c
@@ -1,22 +1,15 @@
 #import "../../compiler/lib/zcommon.bcs"
-#library "EXP_BASE"
+#library "MAIN"
 
 
-strict namespace
+
+// This script runs on map load(should have a unique name for each expansion set
+Script "LEXICON_BASE_EXPANSION" Open
 {
-	// This script runs on map load(should have a unique name for each expansion set
-	Script "LEXICON_BASE_EXPANSION" Open
+	// only run on the hub map
+	if(GetLevelInfo(LEVELINFO_LEVELNUM) == 99)
 	{
-		// only run on the hub map
-		if(GetLevelInfo(LEVELINFO_LEVELNUM) == 99)
-		{
-			// tell lexicon to add the mapset with acronym "DOOM"
-			ACS_NamedExecute("Leixcon_AddMapSet", 0, "DOOM");
-
-			// repeat for each mapset in the expansion
-			//ACS_NamedExecute("Leixcon_AddMapSet", 0, "DOM2");
-			//ACS_NamedExecute("Leixcon_AddMapSet", 0, "MAYH");
-			//...
-		}
+		// tell lexicon to add the mapset with acronym "DOOM"
+		ACS_NamedExecute("Lexicon_AddMapSet", 0, "EPIC");
 	}
 }
