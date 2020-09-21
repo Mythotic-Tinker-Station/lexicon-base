577faf0f6a5a314bbfaba230e618e17579a04ecc
Tribeam <57662310+Tribeam@users.noreply.github.com>
2020-09-21 01:32:08 -0500

    Updated dev branch with new code.

--- pk3/ACS/main.c
+++ pk3/ACS/main.c
@@ -1,22 +1,21 @@
 #import "../../compiler/lib/zcommon.bcs"
-#library "EXP_BASE"
+#library "MAIN"
 
+#if 1
+#define ACRONYM_COUNT 1
 
-strict namespace
+int acronym_list[ACRONYM_COUNT] =
 {
-	// This script runs on map load(should have a unique name for each expansion set
-	Script "LEXICON_BASE_EXPANSION" Open
-	{
-		// only run on the hub map
-		if(GetLevelInfo(LEVELINFO_LEVELNUM) == 99)
-		{
-			// tell lexicon to add the mapset with acronym "DOOM"
-			ACS_NamedExecute("Leixcon_AddMapSet", 0, "EPIC");
+	"MAYH"
+};
 
-			// repeat for each mapset in the expansion
-			//ACS_NamedExecute("Leixcon_AddMapSet", 0, "DOM2");
-			//ACS_NamedExecute("Leixcon_AddMapSet", 0, "MAYH");
-			//...
-		}
+// This script runs on map load(should have a unique name for each expansion set
+Script "LEXICON_BASE_EXPANSION" Open
+{
+	for(int i = 0; i < ACRONYM_COUNT; i++)
+	{
+		ACS_NamedExecute("Lexicon_AddMapSet", 0, acronym_list[i]);
 	}
 }
+
+#endif
