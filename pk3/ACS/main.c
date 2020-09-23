5fdb86a7bc24c495d69d1e436dfe90867a79fc12
Tribeam <57662310+Tribeam@users.noreply.github.com>
2020-09-23 16:33:18 -0500

    Started work on Mayhem 17.

--- pk3/ACS/main.c
+++ pk3/ACS/main.c
@@ -3,15 +3,17 @@
 
 #if 1
 
-#define ACRONYM_COUNT 2
+#define ACRONYM_COUNT 3
 
 int acronym_list[ACRONYM_COUNT] =
 {
 	"EPIC",
-	"EPC2"
+	"EPC2",
+	"MAYH",
 };
 
 // This script runs on map load(should have a unique name for each expansion set
+// you do not need to edit this script, only the variables above
 Script "LEXICON_BASE_EXPANSION" Open
 {
 	for(int i = 0; i < ACRONYM_COUNT; i++)
