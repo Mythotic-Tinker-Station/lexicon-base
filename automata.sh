cb99f73ce1b453d26162154532dd417d8b819dec
Mathew Antich <mifu@allfearthesentinel.net>
2021-05-09 14:44:49 +0800

    Fixed some typos preventing sucessfull build

--- automata.sh
+++ automata.sh
@@ -29,11 +29,11 @@ GAME_PATH='./pk3'
 
 # Compile BCS Code
 chmod +x $COMPILE_PATH/bcc
-$COMPILE_PATH/bcc -acc-err-file -x bcs $GAME_PATH/Acs/main.c $GAME_PATH/Acs/main.o
+$COMPILE_PATH/bcc -acc-err-file -x bcs $GAME_PATH/ACS/main.c $GAME_PATH/ACS/main.o
 
 # Pack game files
 cd $GAME_PATH
-7za a -r -ssw -mx9 -tzip  ../${FILE_NAME}.pk3 ./*.kvx ./*.x* ./*.txt ./*.o ./*.png ./*.wad ./*.md2 ./*.md3 ./*.tga ./*.mp3 ./*.pal ./*.bmp ./*.map ./*.lmp ./*.raw ./*.ogg ./*.vgz ./*.wav ./*.pk3 ./*.gl ./*.mid ./*.dmx
+7za a -r -ssw -mx9 -tzip  ../${FILE_NAME}.pk3 *.kvx *.x* *.txt *.o *.png *.wad *.md2 *.md3 *.tga *.mp3 *.pal *.bmp *.map *.lmp *.raw *.ogg *.vgz *.wav *.pk3 *.gl *.mid *.dmx
 
 # Create proper Jenkins filenames
 cd ..
