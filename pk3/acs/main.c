28710709eb5f0e11ad835f1063ee2b288169c82c
Tribeam <57662310+Tribeam@users.noreply.github.com>
2023-05-06 21:33:42 -0500

    Made pk3 specific folders lowercase.

---
+++ pk3/acs/main.c
@@ -0,0 +1,42 @@
+#import "../../compiler/lib/zcommon.bcs"
+#library "MAIN"
+
+#if 1
+
+#define MODCOUNT 19
+int acronym_list[MODCOUNT] =
+{
+	"EPIC",
+	"EPC2",
+	"MAYH",
+	"SHIT",
+	"CORE",
+	"DBP1",
+	"DBP2",
+	"DBP3",
+	"CCH1",
+	"CCH2",
+	"CCH3",
+	"CCH4",
+	"D2RL",
+	"HEBO",
+	"JPCP",
+	"3HA1",
+	"3HA2",
+	"3HA3",
+	"D2RD",
+};
+
+// This script runs on map load(should have a unique name for each expansion set
+// you do not need to edit this script, only the variables above
+Script "LEXICON_BASE_EXPANSION" Open
+{
+	for(int i = 0; i < MODCOUNT; i++)
+	{
+		ACS_NamedExecuteWithResult("Lexicon_AddMapSet", acronym_list[i]);
+	}
+}
+
+#endif
+
+
