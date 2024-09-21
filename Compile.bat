494d6c6cbbf001fdd68b2be249b94657ae481710
Mathew Antich <mifu@mythotic.com>
2024-09-21 11:18:26 +0800

    Syncing mapset branch (#40)
    * Use TEXTURES lump for Stardate 20X7 textures
    
    * LEX-166 Hell In Hell Maps
    
    * LEX-166 credits
    
    * Cleaned up Hell in Hell mapinfo
    
    -Removed the cluster and par properties since those aren't needed
    
    * Convert single patch texture definitions to graphics
    
    * Added missing SDX7 intermission screen
    
    * Updated Alien Vendetta
    
    * Added Speed of Doom
    
    * Re-added everything from the deleted branch
    
    -Re-added everything from the deleted branch because I'm an idiot who doesn't check if a branch is in development or not
    
    * Added Doom Core Trilogy mapinfo
    
    * Band-aid fix for missing switch texture
    
    * LEX-186 Added Linux Compile Script
    
    * Added missing decaldef for one actor
    
    * Added a list of all the mapset acronyms for later use.
    
    * Fixed Doom Core Trilogy intermission graphic
    
    * Removed colormaps folder
    
    * Added Claustrophobia 1024 2
    
    -Added Claustrophobia 1024 2, plus added language definitions and credits for the mapsets in the rebuild process
    
    * LEX-66 Added Unholy Realms Assets
    
    * LEX-66 Unholy Realms Added MAPINFO
    
    * Finished Unholy Realms
    
    * Added Unholy Realms credits
    
    * Fixed misnamed mapinfo
    
    * Re-extracted Speed of Doom
    
    -Used the wad with BLOODWOLF's coopfixes instead of the regular wad
    
    * Added Whispers of Satan
    
    * Added Vile Flesh
    
    * Added Doom II: 25 Years on Earth
    
    * Renamed all entries to more descriptive names.
    
    * Changed some names of things.
    
    * Update language.txt
    
    * Added Scythe 1
    
    * Update Compile.bat
    
    * Updated compile script
    
    * Added Hadephobia
    
    * Fixed HADP30 calling for the wrong music file
    
    * Added Going Down
    
    * Added Scythe 2
    
    * Added UAC Ultra
    
    * Added Hellcore 2.0
    
    * Added Revolution!
    
    * Fixed Hellcore 2.0 and Revolution! MAPINFO
    
    -Fixed being able to jump or crouch in Hellcore 2.0 and Revolution!
    
    * Added Epic 2
    
    * Fixed Epic 2 enemy drops
    
    -Fixed zombies in Epic 2 not dropping the proper items
    
    * Added Perdition's Gate
    
    * Added Hell to Pay
    
    * Made note of certain Hell to Pay maps
    
    -Made note of certain Hell to Pay maps that have ghost monsters in them
    
    * Added Ancient Aliens sounds and sprites
    
    * Finished implementing Ancient Aliens actors
    
    * Fixed Epic 2 BossEye behavior
    
    * Almost done converting Ancient Aliens
    
    -I need to figure out how to get the script working
    
    * Finished getting Ancient Alien level scripts working
    
    * Added Doom II: Dark World
    
    * Added untouched thumbnails for base pack
    
    * Fixed transparency issue with earth.wad textures
    
    * Shrunk down thumbnails
    
    * Added NeoDoom
    
    * Swapped a couple NeoDoom flats around
    
    ---------
    
    Co-authored-by: Popsoap <popsoap10@gmail.com>
    Co-authored-by: Tabijaky <73826188+Tabijaky@users.noreply.github.com>
    Co-authored-by: cubebert-github <cubebertoffical@gmail.com>
    Co-authored-by: Mathew Antich <mifu@retrosurgegaming.xyz>
    Co-authored-by: Tribeam <57662310+Tribeam@users.noreply.github.com>
    Co-authored-by: BathySalts <bathysalts@gmail.com>

--- Compile.bat
+++ Compile.bat
@@ -2,14 +2,45 @@
 
 set FileName=Lexicon-Base
 
-echo Step 1: PACK
-	cd pk3
-	..\compiler\7za a -r -ssw -mx9 -tzip  ..\%FileName%-New.pk3 *.kvx *.cmp *.x* *.txt *.o *.acs *.png *.wad *.md2 *.md3 *.tga *.mp3 *.pal *.bmp *.map *.lmp *.raw *.mus *.mid *.ogg *.vgz *.wav *.mod *.it *.xm *.s3m *.psm *.pk3 *.gl
+echo 1. Compile ACS
+echo 2. Pack
+echo 3. Both
 
+set /p choice=Enter your choice (1-3): 
 
-echo Step 2: REPLACE
-	cd ..
-	del %FileName%.pk3
-	ren %FileName%-New.pk3 %FileName%.pk3
+if %choice%==1 (
+    echo Step 1: ACS
+    compiler\bcc.exe -acc-stats -acc-err-file -x bcs pk3\acs\LxBase.acs pk3\acs\LxBase.o
+    if not exist pk3\acs\acs.err goto 20
+    del pk3\acs\acs.err
+    pause
+)
 
-pause
+if %choice%==2 (
+    echo Step 2: PACK
+    cd pk3
+    ..\compiler\7za a -r -ssw -mx9 -tzip  ..\%FileName%-New.pk3 *.kvx *.cmp *.x* *.txt *.o *.acs *.png *.wad *.md2 *.md3 *.tga *.mp3 *.pal *.bmp *.map *.lmp *.raw *.mus *.mid *.ogg *.vgz *.wav *.mod *.it *.xm *.s3m *.psm *.pk3 *.gl
+    
+    echo Step 3: REPLACE
+    cd ..
+    del %FileName%.pk3
+    ren %FileName%-New.pk3 %FileName%.pk3
+    pause
+)
+
+if %choice%==3 (
+    echo Step 1: ACS
+    compiler\bcc.exe -acc-stats -acc-err-file -x bcs pk3\acs\LxBase.acs pk3\acs\LxBase.o
+    if not exist pk3\acs\acs.err goto 20
+    del pk3\acs\acs.err
+
+    echo Step 2: PACK
+    cd pk3
+    ..\compiler\7za a -r -ssw -mx9 -tzip  ..\%FileName%-New.pk3 *.kvx *.cmp *.x* *.txt *.o *.acs *.png *.wad *.md2 *.md3 *.tga *.mp3 *.pal *.bmp *.map *.lmp *.raw *.mus *.mid *.ogg *.vgz *.wav *.mod *.it *.xm *.s3m *.psm *.pk3 *.gl
+
+    echo Step 3: REPLACE
+    cd ..
+    del %FileName%.pk3
+    ren %FileName%-New.pk3 %FileName%.pk3
+    pause
+)
